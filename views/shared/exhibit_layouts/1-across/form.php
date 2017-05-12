551539700fd62798cc42fef4d44efcac77e22398
Arden Kirkland <arden@ardenkirkland.com>
2017-05-11 17:25:41 -0700

    added holding institution to caption options, changes for new layout titles

---
+++ views/shared/exhibit_layouts/1-across/form.php
@@ -0,0 +1,39 @@
+<?php
+$formStem = $block->getFormStem();
+$options = $block->getOptions();
+?>
+<style>
+ .metadata-display > label {
+    display:inline-block;
+ }
+ </style>
+<div class="selected-items">
+    <h4><?php echo __('Items'); ?></h4>
+    <?php echo $this->exhibitFormAttachments($block); ?>
+</div>
+
+<div class="layout-options">
+    <div class="block-header">
+        <h4><?php echo __('Layout Options'); ?></h4>
+        <div class="drawer"></div>
+    </div>
+
+    <div class="metadata-display">
+        <?php echo $this->formLabel($formStem . '[options][metadata-display]', __('Display these elements:')). '<br />'; ?>
+        <?php
+        echo $this->formMultiCheckbox($formStem . '[options][metadata-display]',
+            @$options['metadata-display'], array('listsep' => '&nbsp;'),
+            array(
+                "show-creator" => " Creator",
+                "show-title" => " Title",
+                "show-date" => " Date",
+                "show-medium" => " Medium",
+                "show-extent" => " Extent",
+                "show-holding" => " Holding Institution",
+                ));
+                ?>
+         
+            <p class="instructions"><?php echo __('Check the boxes next to the elements you want to display in the caption area for elements in this block.'); ?></p>
+    </div>
+
+</div>
---
+++ views/shared/exhibit_layouts/1-across/layout.php
@@ -0,0 +1,67 @@
+<?php
+$showMetadata = isset($options['metadata-display'])
+    ? ($options['metadata-display'])
+    : '';
+    
+?>
+
+<div class="gallery">
+	<div style="text-align:left;"><?php echo $this->shortcodes($text); ?></div>
+	<?php foreach ($attachments as $attachment): ?>
+	<?php $item = $attachment->getItem(); ?>
+	<?php $file = $attachment->getFile(); ?>
+		<div id="1-across-row">
+		<div class="exhibit-item exhibit-gallery-item">
+        	<?php $altText = "Thumbnail for item, linking to full sized image."; ?>
+        	<?php if  ($description = (metadata($item, array("Dublin Core", "Description")))): ?>
+        		<?php $altText =  $description; ?>
+        	<?php endif; ?> 
+        <div class="item-file">
+            <?php echo "<a href=".metadata($item, array("Item Type Metadata", "URL")).">".file_image('fullsize', array('class' => 'full', 'alt' => "$altText", 'title' => metadata($item, array("Dublin Core", "Title"))), $file)."</a>"; ?>
+        </div>
+
+           	<?php if ($attachment['caption'] || !empty($showMetadata)): ?>
+            	<div class="exhibit-item-caption">
+            		<?php 
+                    if ($attachment['caption']) {echo $attachment['caption']; }
+
+                    if (!empty($showMetadata)) {
+
+                        if (in_array("show-creator", $showMetadata)) { 
+                            echo "<div class='exhibit-item-title'>"
+                            .metadata($item, array("Dublin Core", "Creator"), array('snippet'=>100))."</div>"; 
+                        }    
+                        if (in_array("show-title", $showMetadata)) { 
+                            echo "<div class='exhibit-item-title'><a href="
+                            .metadata($item, array("Item Type Metadata", "URL")).">".metadata($item, array("Dublin Core", "Title"), 
+                                array('snippet'=>100))."</a></div>"; 
+                        }                   
+                        if (in_array("show-date", $showMetadata)) { 
+                            echo "<div class='exhibit-item-date'>"
+                            .metadata($item, array("Dublin Core", "Date"), array('snippet'=>100))."</div>";
+                        }
+                        if (in_array("show-medium", $showMetadata)) { 
+                            echo '<div class="exhibit-item-description">'
+                            .metadata($item, array("Dublin Core", "Medium"), array('snippet'=>150))."</div>";
+                        }
+                        if (in_array("show-extent", $showMetadata)) { 
+                            echo "<div class='exhibit-item-description'>"
+                            .metadata($item, array("Dublin Core", "Extent"),array('snippet'=>150))."</div>"; 
+                        }
+                        if (in_array("show-holding", $showMetadata)) { 
+                            echo "<div class='exhibit-item-description'>"
+                            .metadata($item, array("Item Type Metadata", "Holding Institution"),array('snippet'=>150))."</div>"; 
+                        }
+                    }
+
+                	; ?>
+            	</div>
+
+            <?php endif; ?>
+        	</div>
+         </div>
+        <?php endforeach; ?>   
+
+    </div>
+</div>
+
